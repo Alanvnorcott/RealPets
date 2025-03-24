2777c405d05c42a03208f886d3d0aa7ed4fb9bb5
Alanvnorcott <alanvnorcott@gmail.com>
2025-03-24 18:42:44 -0400

    NEW

--- RealPets.lua
+++ RealPets.lua
@@ -1,5 +1,9 @@
-print("Go fuck yourself!")
+print("RealPets: Addon loaded!")
+if not RealPetsDB then
+    RealPetsDB = {}
+end
 local mainFrame = CreateFrame("Frame", "RealPetsMainFrame", UIParent, "BasicFrameTemplateWithInset")
+local eventListenerFrame = CreateFrame("Frame", "RealPetsEventListenerFrame", UIParent)
 mainFrame:SetSize(500, 350)
 mainFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
 mainFrame.TitleBg:SetHeight(30)
@@ -35,4 +39,16 @@ SlashCmdList["MYADDON"] = function()
     end
 end
 
+local function eventHandler(self, event, ...)
+    if event == "PLAYER_REGEN_ENABLED" then
+        print("RealPets: " .. UnitName("pet") .. " has exited combat")
+    end
+end
+
+eventListenerFrame:SetScript("OnEvent", eventHandler)
+eventListenerFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
 table.insert(UISpecialFrames, "RealPetsMainFrame")
+
+mainFrame.petName = mainFrame.CreateFontString(nil, "OVERLAY", "GameFontNormal")
+mainFrame.petName:SetPoint("TOPLEFT", mainFrame, "TOPLEFT", 15, -35)
+mainFrame.petName:SetText("Bestfriend: " .. UnitName("pet") .." (Level " .. UnitLevel("pet") .. ")")
